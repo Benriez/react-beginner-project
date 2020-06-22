f3aad722f6cc302c1ad9d781e8134d7959709252
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-22 16:55:25 +0200

    added changeAvatar function

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 import Card from './components/card';
 
 class App extends React.Component {
+  
+  state = {
+    avatarId: 50
+  }
+
+  changeAvatar = () => {
+    this.setState({
+      avatarId: 43
+    })
+  }
+
   render () {
     return (
       <div className="container">
         <div className="row">
-          <Card name="Rick Sanchez" job="scientist" avatarId={50} />
+          <Card name="Rick Sanchez" job="scientist" avatarId={this.state.avatarId} />
           <Card name="Morty Smith" job="student" avatarId={28} />
           <Card name="Gerry Smith" job="eat apples" avatarId={47} />
+        </div>
+        <div className="row">
+          <div className="col-md-3">
+            <a onClick={this.changeAvatar} className="changeAvatar" href="#">Change Avatar</a>
+          </div>
         </div>
       </div>
     );
