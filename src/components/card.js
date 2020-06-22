f1ddad9a3b4211298526bcbbc45585a8e368411d
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-22 16:12:56 +0200

    giving card name attribute

--- src/components/card.js
+++ src/components/card.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const card = () => {
+const card = (props) => {
     return(
-        <h1>Jo</h1>
+        <div className="col-md-4">
+            <div className="card">
+                <img className="card-img-top" src="https://i.pravatar.cc/150?img=47" alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">{props.name}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
     );
 }
 
