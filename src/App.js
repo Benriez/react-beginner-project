409ccc6c69c831a0d8fb508ecb2cad89c70837c6
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-22 17:02:13 +0200

    added personName in state

--- src/App.js
+++ src/App.js
@@ -5,26 +5,31 @@ import Card from './components/card';
 class App extends React.Component {
   
   state = {
-    avatarId: 50
+    avatarId: 50,
+    personName: 'Rick Sanchez'
   }
 
   changeAvatar = () => {
     this.setState({
-      avatarId: 43
+      avatarId: 43,
+      personName: "Summer Smith"
     })
   }
+
+
+
 
   render () {
     return (
       <div className="container">
         <div className="row">
-          <Card name="Rick Sanchez" job="scientist" avatarId={this.state.avatarId} />
+          <Card name={this.state.personName} job="scientist" avatarId={this.state.avatarId} />
           <Card name="Morty Smith" job="student" avatarId={28} />
           <Card name="Gerry Smith" job="eat apples" avatarId={47} />
         </div>
         <div className="row">
           <div className="col-md-3">
-            <a onClick={this.changeAvatar} className="changeAvatar" href="#">Change Avatar</a>
+            <a onClick={this.changeAvatar} className="changeAvatar" href="#">Change Details</a>
           </div>
         </div>
       </div>
