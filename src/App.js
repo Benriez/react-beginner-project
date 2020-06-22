2c4841f0d80abf2a6e00e5027c20c0aaf367713d
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-22 15:53:45 +0200

    added 2 more cards; padding-top: 10px

--- src/App.js
+++ src/App.js
@@ -6,9 +6,29 @@ class App extends React.Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-md4">
+          <div className="col-md-4">
             <div className="card">
-              <img className="card-img-top" src="https://i.pravatar.cc/150?img=47" alt="Card image cap" />
+              <img className="card-img-top" src="https://i.pravatar.cc/150?img=47" alt="" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="https://i.pravatar.cc/150?img=47" alt="" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="https://i.pravatar.cc/150?img=47" alt="" />
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
