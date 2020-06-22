c5e3df96fb051328e9c7146fc7f7874d94db0364
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-22 15:44:54 +0200

    added bootstrap card and convert it to jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import React from 'react';
 class App extends React.Component {
   render () {
     return (
-      <h1>Was geht</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md4">
+            <div className="card">
+              <img className="card-img-top" src="https://i.pravatar.cc/150?img=47" alt="Card image cap" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
