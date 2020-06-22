841804145fa086390ea38928a44d9dfa33553f4a
Benriez <33022072+Benriez@users.noreply.github.com>
2020-06-22 16:03:16 +0200

    import Card in App.js; invoke Card component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Card from './components/card';
 
 class App extends React.Component {
   render () {
@@ -17,24 +18,10 @@ class App extends React.Component {
             </div>
           </div>
           <div className="col-md-4">
-            <div className="card">
-              <img className="card-img-top" src="https://i.pravatar.cc/150?img=47" alt="" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
+            <Card />
           </div>
           <div className="col-md-4">
-            <div className="card">
-              <img className="card-img-top" src="https://i.pravatar.cc/150?img=47" alt="" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
+            <Card />
           </div>
         </div>
       </div>
---
+++ src/components/card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const card = () => {
+    return(
+        <h1>Jo</h1>
+    );
+}
+
+export default card;
