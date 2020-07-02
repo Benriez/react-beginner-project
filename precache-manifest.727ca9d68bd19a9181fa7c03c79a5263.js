1803b82268f28891251b0c5a43b717d39d4adfe6
Benriez <stberiez@stud.h-da.de>
2020-07-02 15:17:01 +0200

    Updates

---
+++ precache-manifest.727ca9d68bd19a9181fa7c03c79a5263.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff33247fbcc9d7d9a19a7ae5158a80f",
+    "url": "/react-beginner-project/index.html"
+  },
+  {
+    "revision": "519a32d8a46aaee35651",
+    "url": "/react-beginner-project/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "35abf25018b3524050bc",
+    "url": "/react-beginner-project/static/css/main.9fa0f7fc.chunk.css"
+  },
+  {
+    "revision": "519a32d8a46aaee35651",
+    "url": "/react-beginner-project/static/js/2.4e4b87cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-beginner-project/static/js/2.4e4b87cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35abf25018b3524050bc",
+    "url": "/react-beginner-project/static/js/main.1e782785.chunk.js"
+  },
+  {
+    "revision": "8a9c2d4bbe546818c79c",
+    "url": "/react-beginner-project/static/js/runtime-main.096d4313.js"
+  }
+]);
